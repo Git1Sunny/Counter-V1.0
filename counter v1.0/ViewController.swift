6f98671d022d5a0871705623b2f2df2e15e4f74f
Git1Sunny <764152162@qq.com>
2018-10-12 19:32:26 +0800

    Initial Commit

---
+++ counter v1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  counter v1.0
+//
+//  Created by S20171102171 on 2018/10/12.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
